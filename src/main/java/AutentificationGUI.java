1bfe3947e62d46c4578948448fb1fe9d3affc962
Paul <paulb2233@yahoo.com>
2020-05-06 13:57:46 +0300

    ARS-43 Creare Project setup si Repository

---
+++ src/main/java/AutentificationGUI.java
@@ -0,0 +1,24 @@
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class AutentificationGUI {
+
+    private JFrame frame;
+    public AutentificationGUI() {
+        frame = new JFrame();
+        frame.setBounds(100, 100, 450, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    AutentificationGUI window = new AutentificationGUI();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
