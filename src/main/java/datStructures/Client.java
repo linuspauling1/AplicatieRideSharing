8e707f4e55f680057549930b585865aa980f1e56
Catalin <catabotean@gmail.com>
2020-05-10 12:46:14 +0300

    ARS-38 Creare Clasa Client

---
+++ src/main/java/datStructures/Client.java
@@ -0,0 +1,28 @@
+package datStructures;
+
+public class Client extends User{
+        private String Adresa;
+        private String telefon;
+        public Client(String username, String password){
+            super(username,password);
+        }
+        public void setTelefon(String telefon) {
+            this.telefon = telefon;
+        }
+        public void setAdresa(String adresa) {
+            Adresa = adresa;
+        }
+        public String getAdresa() {
+            return Adresa;
+        }
+        public String getTelefon() {
+            return telefon;
+        }
+        @Override
+        public String toString() {
+            return "Client{" +
+                    "Adresa='" + Adresa + '\'' +
+                    ", telefon='" + telefon + '\'' +
+                    '}';
+        }
+}
